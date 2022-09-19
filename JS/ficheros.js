7b3aa2d6de6f2dc9d8145ed350f4c2bab5f61e28
successteam10 <afballesterosl@gmail.com>
2022-09-18 19:53:57 -0500

    Boton crear y visualizar valor total

---
+++ JS/ficheros.js
@@ -0,0 +1,11 @@
+var getData = function () {
+    var fecha = document.getElementById("fecha").value;
+    var cantidad = document.getElementById("cantidad").value;
+    var ValorUnitario = document.getElementById("valor-unitario").value;
+    var cliente = document.getElementById("cliente").value;
+    var producto = document.getElementById("Producto").value;
+    var valorTotal = cantidad * ValorUnitario;
+    //console.log(fecha + " " + cantidad + " " + ValorUnitario + " " + cliente + " " + producto + " " + valorTotal)
+    window.alert("Venta Creada con Exito, Valor Total: $" + valorTotal);
+
+}
