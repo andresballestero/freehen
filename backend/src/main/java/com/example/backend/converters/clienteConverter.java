b90e2a2e0dbc70ad44ba1be799a3883e09903179
successteam10 <afballesterosl@gmail.com>
2022-10-04 15:50:40 -0500

    demas carpetas

---
+++ backend/src/main/java/com/example/backend/converters/clienteConverter.java
@@ -0,0 +1,5 @@
+package com.example.backend.converters;
+
+public class clienteConverter {
+
+}
---
+++ backend/src/main/java/com/example/backend/converters/converter.java
@@ -0,0 +1,22 @@
+package com.example.backend.converters;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class converter<E, D> {
+    public abstract E toEntity(D object);
+
+    public abstract D toData(E object);
+
+    public List<E> toEntity(List<D> list) {
+        return list.stream()
+                .map(object -> toEntity(object))
+                .collect(Collectors.toList());
+    }
+
+    public List<D> toData(List<E> list) {
+        return list.stream()
+                .map(object -> toData(object))
+                .collect(Collectors.toList());
+    }
+}
