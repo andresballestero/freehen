2be8a883f1415d8fad56637c4da034c9ec6fbd11
successteam10 <afballesterosl@gmail.com>
2022-10-04 20:14:28 -0500

    avance converte

--- backend/src/main/java/com/example/backend/converters/clienteConverter.java
+++ backend/src/main/java/com/example/backend/converters/clienteConverter.java
@@ -1,5 +1,27 @@
 package com.example.backend.converters;
 
-public class clienteConverter {
+import com.example.backend.data.clienteData;
+import com.example.backend.entities.cliente;
 
+public class clienteConverter extends converter<cliente, clienteData> {
+
+    @Override
+    public cliente toEntity(clienteData object) {
+        return object == null ? null
+                : cliente.builder()
+                        .id(object.getId())
+                        .nombreCliente(object.getNombreCliente())
+                        .celularCliente(object.getCelularCliente())
+                        .build();
+    }
+
+    @Override
+    public clienteData toData(cliente object) {
+        return object == null ? null
+                : clienteData.builder()
+                        .id(object.getId())
+                        .nombreCliente(object.getNombreCliente())
+                        .celularCliente(object.getCelularCliente())
+                        .build();
+    }
 }
