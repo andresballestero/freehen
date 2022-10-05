03226325cdbd692624137fd2a96ff5d9858b0aad
successteam10 <afballesterosl@gmail.com>
2022-10-04 19:34:58 -0500

    convert cliente- producto creados

--- backend/src/main/java/com/example/backend/converters/productoConverter.java
+++ backend/src/main/java/com/example/backend/converters/productoConverter.java
@@ -1,5 +1,30 @@
 package com.example.backend.converters;
 
-public class productoConverter {
+import com.example.backend.data.productosData;
+import com.example.backend.entities.productos;
+
+public class productoConverter extends converter<productos, productosData> {
+
+    @Override
+    public productos toEntity(productosData object) {
+        return object == null ? null
+                : productos.builder()
+                        .id(object.getId())
+                        .cantidad(object.getCantidad())
+                        .precioUnitario(object.getPrecioUnitario())
+                        .nombreProducto(object.getNombreProducto())
+                        .build();
+    }
+
+    @Override
+    public productosData toData(productos object) {
+        return object == null ? null
+                : productosData.builder()
+                        .id(object.getId())
+                        .cantidad(object.getCantidad())
+                        .precioUnitario(object.getPrecioUnitario())
+                        .nombreProducto(object.getNombreProducto())
+                        .build();
+    }
 
 }
