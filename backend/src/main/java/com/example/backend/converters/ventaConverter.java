6003e12d1438b5374ece1caa54155f5a57ad4529
successteam10 <afballesterosl@gmail.com>
2022-10-04 21:14:55 -0500

    Ventaconverter no da

--- backend/src/main/java/com/example/backend/converters/ventaConverter.java
+++ backend/src/main/java/com/example/backend/converters/ventaConverter.java
@@ -1,5 +1,35 @@
 package com.example.backend.converters;
 
-public class ventaConverter {
+import com.example.backend.data.ventaData;
+import com.example.backend.entities.venta;
+
+public class ventaConverter extends converter<venta, ventaData> {
+
+    private clienteConverter clienteConverter = new clienteConverter();
+    private productoConverter productoConverter = new productoConverter();
+
+    @Override
+    public venta toEntity(ventaData object) {
+        return object == null ? null
+                : venta.builder()
+                        .id(object.getId())
+                        .cliente(clienteConverter.toEntity(object.getCliente()))
+                        .productos(productoConverter.toEntity(object.getProductos()))
+                        .fecha(object.getFecha())
+                        .build();
+
+    }
+
+    @Override
+    public ventaData toData(venta object) {
+        return object == null ? null
+                : ventaData.builder()
+                        .id(object.getId())
+                        .cliente(clienteConverter.toData(object.getCliente()))
+                        .productos(productoConverter.toData(object.getProductos()))
+                        .fecha(object.getFecha())
+                        .build();
+
+    }
 
 }
